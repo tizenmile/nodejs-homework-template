9811d078aa9115c3f1018dda19c07bb43e9c749a
unknown <tizen48@gmail.com>
2022-12-26 16:41:02 +0200

    initial commit

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -76,16 +76,17 @@ const updateContactFavorite = async (req, res) => {
   const contactId = req.params.contactId;
   const body = req.body;
   if (body.favorite.length) {
-    res.status(200).json({ message: "missing field favorite" });
+    res.status(400).json({ message: "missing field favorite" });
+    return;
   }
   try {
     await Contact.updateOne({ _id: contactId }, { favorite: body.favorite });
     res.status(200).json({ status: "success", code: 200, data: body });
   } catch (error) {
-    res.status(400).json({
+    res.status(404).json({
       status: "failed",
-      code: 400,
-      data: { message: "missing field favorite" },
+      code: 404,
+      data: { message: "Not found" },
     });
   }
 };
